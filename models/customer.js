b9260db3ca6d9a6173951c283d99bdbd0dde096e
wasadmin <wasadmin@wasadmin.com>
2022-07-01 15:22:48 -0500

    remove github token from file

--- models/customer.js
+++ models/customer.js
@@ -2,8 +2,6 @@ import mongoose from 'mongoose'
 import s from 'mongoose-sequence'
 const sequence = s(mongoose)
 
-// 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
-
 const { Schema, model } = mongoose
 
 const customerSchema = new Schema({
