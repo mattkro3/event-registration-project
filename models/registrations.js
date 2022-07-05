e452e107126feaf374694d8e87af37a9cea9898e
wasadmin <wasadmin@wasadmin.com>
2022-07-05 10:15:32 -0500

    datacleaner

---
+++ models/registrations.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose'
+import s from 'mongoose-sequence'
+const sequence = s(mongoose)
+
+const { Schema, model } = mongoose
+
+const registrationsSchema = new Schema({
+    
+    REGISTRATIONS_ID:Number,
+    REGISTRATIONS_DATE: Number,
+    NOTES: String,
+    EVENT_ID:Number,
+    CUSTOMER_ID:Number,
+
+})
+
+registrationsSchema.plugin(sequence, {inc_field: ' REGISTRATIONS_ID'}) 
+
+const registrations = model('registraions', registrationsSchema )
+export default registrations
