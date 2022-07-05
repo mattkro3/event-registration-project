a772c586ad9ddcf6ca63d728704cab619f96a74a
wasadmin <wasadmin@wasadmin.com>
2022-07-05 10:51:52 -0500

    added event routes and updated server.js to include event / registration routes

--- routes/event-routes.js
+++ routes/event-routes.js
@@ -1,7 +1,25 @@
-  // GET /api/events
+import express from 'express'
+var eventRouter = express.Router()
+import eventController from '../controllers/event-controller.js'
 
-  // GET /api/events/:event_id
+// GET /api/events
+eventRouter.get('/', function(req, res) {
+  eventController.getAllEvents(req, res)
+})
 
-  // POST /api/events
+// GET /api/events/:event_id
+eventRouter.get('/:event_id', function(req, res) {
+  eventController.getEventByID(req, res)
+})
 
-  // DELETE /api/events/:event_id
+// POST /api/events
+eventRouter.post('/', function(req, res) {
+  eventController.postEvent(req, res)
+})
+
+// DELETE /api/events/:event_id
+eventRouter.delete('/:event_id', function(req, res) {
+  eventController.deleteEventByID(req, res)
+})
+
+export default eventRouter
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import init_db from './utilities/initialize.js'
 import express from 'express'
 import accountRouter from './routes/account-routes.js'
 import customerRouter from './routes/customer-routes.js'
+import eventRouter from './routes/event-routes.js'
+import registrationRouter from './routes/registration-routes.js'
 
 init_db()
 
@@ -10,6 +12,8 @@ app.use(express.json())
 
 app.use('/api/accounts', accountRouter)
 app.use('/api/customers', customerRouter)
+app.use('/api/events', eventRouter)
+app.use('/api/registrations', registrationRouter)
 
 app.listen('8000');
 
